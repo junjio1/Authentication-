45a286bc755da0b0da9c5403b9c95f408b231d2d
Junji Oshiro <junjio1@hotmail.com>
2021-02-13 15:33:05 -0300

    add submit function

--- app.js
+++ app.js
@@ -39,7 +39,8 @@ const userSchema = new mongoose.Schema({
     email:String,
     password: String,
     googleId: String,
-    facebookId: String
+    facebookId: String,
+    secret:String
 }) 
 
 userSchema.plugin(passportLocalMongoose)
@@ -80,7 +81,7 @@ passport.use(new FacebookStrategy({
     callbackURL: "http://localhost:3000/auth/facebook/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
-    console.log(profile)
+    
     User.findOrCreate({ facebookId: profile.id }, function (err, user) {
       return cb(err, user);
     });
@@ -123,11 +124,41 @@ app.get("/register" ,(req,res) =>{
 })
 
 app.get("/secrets", (req ,res)=>{
-    if(req.isAuthenticated()){
-        res.render("secrets")
+  User.find({"secret": {$ne: null}}, (err, foundUsers)=>{
+    if(err){
+      console.log(err)
+    }else{
+      if(foundUsers){
+        res.render("secrets", {userWithSecrets: foundUsers})
+      }  
+    }
+  })
+})
+
+app.get("/submit", (req, res) =>{
+      if(req.isAuthenticated()){
+        res.render("submit")
     }else{
         res.redirect("/login")
     }
+})
+
+app.post("/submit", (req, res)=>{
+    const submittedSecret = req.body.secret
+    
+    console.log(req.user)
+
+    User.findById(req.user.id, (err, foundUser)=>{
+      if(err){
+        console.log(err)
+      }else{
+        if(foundUser){
+          foundUser.secret = submittedSecret
+          foundUser.save()
+          res.redirect("/secrets")
+        }
+      }
+    })
 })
 
 app.get("/logout", (req,res)=>{
